e2c58b5aca2e7e41cb53764ed3163e8cb8af69ce
Gismet <93595721+Gismet@users.noreply.github.com>
2022-09-18 10:08:58 +0400

    Add files via upload

---
+++ MiniProj3.java
@@ -0,0 +1,15 @@
+public class MiniProj3 {
+    public static void main(String[] args) {
+        int number = 6;
+        int total = 0;
+
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0)
+                total += i;
+        }
+        if (total == number)
+            System.out.println("The number is perfect number");
+        else
+            System.out.println("The number is not perfect number");
+    }
+}
